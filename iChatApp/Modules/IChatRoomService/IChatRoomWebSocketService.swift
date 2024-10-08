8b3117fe36fd2be25a38f9676d5a8f1330d7765c
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-10-08 12:56:36 +0300

    Socket connected successfully.

--- iChatApp/Modules/IChatRoomService/IChatRoomWebSocketService.swift
+++ iChatApp/Modules/IChatRoomService/IChatRoomWebSocketService.swift
@@ -6,24 +6,43 @@ public protocol ChatRoomWebSocketAPI {
 }
 
 public final class IChatRoomWebSocketService {
+    private var socketURL: String
     private var scoketManager: SocketManager!
     private var socket: SocketIOClient!
     
-    public init() { }
-    deinit { socket.disconnect() }
+    public init(socketURL: String) {
+        self.socketURL = socketURL
+        setup(socketURL: URL(string: socketURL)!)
+    }
     
+    deinit { socket.disconnect() }
 }
 
+//  MARK: - ChatRoomWebSocketAPI
 extension IChatRoomWebSocketService: ChatRoomWebSocketAPI {
     public func login(username: String, email: String) {
         print("Login request received", username, email)
+        socket.emit(ChatSocket.Request.login, username, email)
     }
 }
 
-extension IChatRoomWebSocketService {
+enum ChatSocket {
+    struct Request {
+        static let login = "login"
+    }
+    
+    struct Response {
+        static let loginResponse = "loginResponse"
+    }
+}
+
+private extension IChatRoomWebSocketService {
     func setup(socketURL: URL) {
         scoketManager = SocketManager(socketURL: socketURL)
         socket = self.scoketManager.defaultSocket
         socket.connect()
+        socket.on(ChatSocket.Response.loginResponse) { (dataArray, socketAck) in
+            print("Login", dataArray)
+        }
     }
 }
--- iChatApp/Modules/UseCases/UseCasesFactory.swift
+++ iChatApp/Modules/UseCases/UseCasesFactory.swift
@@ -2,6 +2,7 @@ import Foundation
 import IChatRoomService
 
 public final class UseCasesFactory {
-    private static let websocketService = IChatRoomWebSocketService()
+    static let deployURL = "https://damp-beach-81446.herokuapp.com/"
+    private static let websocketService = IChatRoomWebSocketService(socketURL: deployURL)
     public static var accountInteractor: AccountInteractor = AccountInteractor(chatService: websocketService)
 }
